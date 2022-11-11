e9285f79154415a52584ac67b4b360031bf3ff86
TytovAnton <Antont5630@gmail.com>
2022-11-11 11:37:32 +0200

    hw2

---
+++ JS_homeWorks/lesson1_js/main.js
@@ -0,0 +1,58 @@
+let q = "hello";
+console.log(q);
+q = "hello"
+let w = "owu";
+console.log(w);
+w = "owu"
+let e = "com";
+console.log(e);
+e = "com"
+let r = "ua";
+console.log(r);
+r = "ua"
+let t = "1";
+console.log(t);
+t = "1"
+let y = "10";
+console.log(y);
+y = "10"
+let u = "-999";
+console.log(u);
+u = "-999"
+let i = "123";
+console.log(i);
+i = "123"
+let o = "3.14";
+console.log(o);
+o = "3.14"
+let p = "2.7";
+console.log(p);
+p = "2.7"
+let a = "16";
+console.log(a);
+a = "16"
+let s = "true";
+console.log(s);
+s = "true"
+let d = "false";
+console.log(d);
+d = "false"
+
+
+
+
+let firstName = "Anton";
+let middlename = "Tytov";
+let lastName = "Andriyovich";
+
+let person = firstName + ' ' + middlename + ' ' + lastName ;
+console.log(person);
+
+
+
+let z = 100;
+console.log(typeof z);
+let x = '100';
+console.log(typeof x);
+let c = true;
+console.log(typeof c);
---
+++ JS_homeWorks/lesson2_js/main.js
@@ -0,0 +1,132 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+let fruits = ["apple", "pear", "plum", "pineapple", "orange", "cherry", "lemon", "melon", "watermelon", "lime"];
+console.log(fruits[0])
+console.log(fruits[1])
+console.log(fruits[2])
+console.log(fruits[3])
+console.log(fruits[4])
+console.log(fruits[5])
+console.log(fruits[6])
+console.log(fruits[7])
+console.log(fruits[8])
+console.log(fruits[9])
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+let book1 = {
+    title: 'Harry Potter and the Philosophers Stone',
+    pageCount: 223,
+    genre: 'Fantasy'
+}
+console.log(book1)
+
+let book2 = {
+    title: 'Harry Potter and the cursed child',
+    pageCount: 336,
+    genre: 'Fantasy'
+}
+console.log(book2)
+let book3 = {
+    title: 'Harry Potter and the Order of the Phoenix',
+    pageCount: 896,
+    genre: 'Fantasy'
+}
+console.log(book3)
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+let book4 = {
+    title: "Kobzar",
+    pageCount: 1160,
+    genre: "poetry book",
+    authors: [{
+        name: "Taras Shevchenko",
+        age: 47
+    }]
+}
+console.log(book4)
+
+
+let book5 = {
+    title: "Sherlock holmes",
+    pageCount: 1796,
+    genre: "criminal novel",
+    authors: [{
+        name: "Arthur Conan Doyle",
+        age: 71
+    }]
+}
+console.log(book5)
+
+
+let book6 = {
+    title: "Lord of the Rings",
+    pageCount: 1137,
+    genre: "fantasy",
+    authors: [{
+        name: "J. R. R. Tolkien",
+        age: 81
+    }]
+}
+console.log(book6)
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+let user = ["user1", "user2", "user3", "user4", "user5", "user6", "user7", "user8", "user9", "user10"];
+console.log(user)
+let user1 = {
+    name: "J. R. R. Tolkien",
+    username: "Vasya1",
+    password: 1234
+}
+let user2 = {
+    name: "J. R. R. Tolkien",
+    username: "Vasya1",
+    password: 1235
+}
+let user3 = {
+    name: "J. R. R. Tolkien",
+    username: "Vasya1",
+    password: 1236
+}
+let user4 = {
+    name: "J. R. R. Tolkien",
+    username: "Vasya1",
+    password: 1237
+}
+let user5 = {
+    name: "J. R. R. Tolkien",
+    username: "Vasya1",
+    password: 1238
+}
+let user6 = {
+    name: "J. R. R. Tolkien",
+    username: "Vasya1",
+    password: 1239
+}
+let user7 = {
+    name: "J. R. R. Tolkien",
+    username: "Vasya1",
+    password: 1231
+}
+let user8 = {
+    name: "J. R. R. Tolkien",
+    username: "Vasya1",
+    password: 1232
+}
+let user9 = {
+    name: "J. R. R. Tolkien",
+    username: "Vasya1",
+    password: 1233
+}
+let user10 = {
+    name: "J. R. R. Tolkien",
+    username: "Vasya1",
+    password: 12389
+}
+console.log(user1.password)
+console.log(user2.password)
+console.log(user3.password)
+console.log(user4.password)
+console.log(user5.password)
+console.log(user6.password)
+console.log(user7.password)
+console.log(user8.password)
+console.log(user9.password)
+console.log(user10.password)
